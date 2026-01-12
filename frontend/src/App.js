11b8b936a6a3707fd66f63eb79d5af996556c2ba
fidanqorrolli <138937876+fidanqorrolli@users.noreply.github.com>
2026-01-12 14:47:23 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,7 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import WhyUsSection from './components/WhyUsSection';
 import ServicesSection from './components/ServicesSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
-import WhatsAppButton from './components/WhatsAppButton';
-
-function App() {
-  const scrollToContact = () => {
-    const element = document.querySelector('#contact');
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }import React from "react";
-import "./App.css";
-import { Toaster } from 'sonner';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import WhyUsSection from './components/WhyUsSection';
-import ServicesSection from './components/ServicesSection';
-import PortfolioSection from './components/PortfolioSection'; // Neu importiert
+import PortfolioSection from './components/PortfolioSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -35,29 +19,7 @@ function App() {
   };
 
   return (
-    // Hintergrund auf slate-900 geändert für einen modernen Look
     <div className="App bg-slate-900 min-h-screen">
-      <Toaster position="top-right" />
-      <Header />
-      <main>
-        <HeroSection onRequestQuote={scrollToContact} />
-        <ServicesSection onRequestQuote={scrollToContact} />
-        {/* Die neue Galerie zwischen Services und WhyUs eingefügt */}
-        <PortfolioSection /> 
-        <WhyUsSection />
-        <ContactSection />
-      </main>
-      <Footer />
-      <WhatsAppButton />
-    </div>
-  );
-}
-
-export default App;
-  };
-
-  return (
-    <div className="App bg-white min-h-screen">
       <Toaster 
         position="top-right" 
         toastOptions={{
@@ -74,8 +36,9 @@ export default App;
       <Header />
       <main>
         <HeroSection onRequestQuote={scrollToContact} />
-        <WhyUsSection />
         <ServicesSection onRequestQuote={scrollToContact} />
+        <PortfolioSection />
+        <WhyUsSection />
         <ContactSection />
       </main>
       <Footer />
